d2c837f9a54cb569a9c10326acbb6dc33c32da05
Raquel Plaza <>
2018-03-22 15:07:21 +0000

    Navigation component, routing and form for new plan component

--- angular5-dojo/src/app/app.module.ts
+++ angular5-dojo/src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule,
+          MatButtonModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatNativeDateModule,
+          MatDatepickerModule
+} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { NewPlanComponent } from './components/new-plan/new-plan.component';
+import { MyPlansComponent } from './components/my-plans/my-plans.component';
+import { MyTagsComponent } from './components/my-tags/my-tags.component';
+
+const appRoutes: Routes = [
+  { path: '', component: NewPlanComponent},
+  { path: 'newplan', component: NewPlanComponent },
+  { path: 'myplans', component: MyPlansComponent },
+  { path: 'mytags', component: MyTagsComponent },
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NewPlanComponent,
+    MyPlansComponent,
+    MyTagsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
