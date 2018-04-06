90438fcb2da6f310ddb3034caef3bcc559f3ec06
Raquel Plaza <>
2018-04-06 17:19:42 +0100

    Adding unit test for new plan

--- angular5-dojo/src/app/components/new-plan/new-plan.component.spec.ts
+++ angular5-dojo/src/app/components/new-plan/new-plan.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement} from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { NewPlanComponent } from './new-plan.component';
+import { PlansService} from '../../shared/services/plans.service';
+import { ReactiveFormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+import { AlertModule, BsDatepickerModule, TimepickerModule} from 'ngx-bootstrap';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('NewPlanComponent', () => {
   let component: NewPlanComponent;
   let fixture: ComponentFixture<NewPlanComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewPlanComponent ]
+      imports: [ReactiveFormsModule,
+        FormsModule,
+        BsDatepickerModule,
+        TimepickerModule,
+        AlertModule,
+        HttpClientModule,
+        HttpClientTestingModule],
+      declarations: [ NewPlanComponent ],
+      providers: [PlansService]
     })
     .compileComponents();
   }));
@@ -17,9 +34,21 @@ describe('NewPlanComponent', () => {
     fixture = TestBed.createComponent(NewPlanComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    el = fixture.debugElement.query(By.css('input'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('form invalid when empty', () => {
+    expect(component.newPlanForm.valid).toBeFalsy();
+  });
+
+  fit('setting submitted to true, displays the notificatio', () =>{
+    component.submitted = true;
+    fixture.detectChanges();
+    expect(el.nativeElement.submitted).toBeTruthy();
   });
 });
